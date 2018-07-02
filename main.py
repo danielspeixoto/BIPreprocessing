6b7c51fe1c8c5804f9f31b96583ad86147b44905
Daniel Peixoto <danielspeixoto@outlook.com>
2018-07-02 14:49:53 -0300

    ds

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from pandas import DataFrame
+import seaborn as sns
+import matplotlib.pyplot as plt
 import preprocessing
 import plots.pie as pie
 
@@ -8,4 +11,34 @@ dispXSpending = data.groupby(
 
 info = dispXSpending.sum()
 
-pie.plot(info)
+# pie.plot(info)
+
+
+########################################
+n = [[0 for _ in range(7)] for _ in range(24)]
+for index, row in data.iterrows():
+    n[row['HORA_PEDIDO']][row['DIA_PEDIDO']] += 1
+
+df = DataFrame(n, index=range(0, 24, 1), columns=range(0, 7, 1))
+
+# _r reverses the normal order of the color map 'RdYlGn'
+sns.heatmap(df, cmap=plt.get_cmap('gray_r'))
+
+plt.ylabel("Hora")
+plt.xlabel("Dia (Segunda=0)")
+plt.title("Momentos com mais pedidos")
+plt.show()
+
+########################################
+
+# Momentos com mais valores nas vendas
+n = [[0 for _ in range(7)] for _ in range(24)]
+for index, row in data.iterrows():
+    n[row['HORA_PEDIDO']][row['DIA_PEDIDO']] += row['VALOR_PRODUTOS']
+
+df = DataFrame(n, index=range(0, 24, 1), columns=range(0, 7, 1))
+
+# _r reverses the normal order of the color map 'RdYlGn'
+sns.heatmap(df, cmap=plt.get_cmap('gray_r'))
+
+plt.show()
---
+++ plots/parallel.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+def plot(info):
+    # Data to plot
+    labels = info.index
+    # Plot
+    patches, texts = plt.pie(info)
+    plt.legend(patches, labels, loc="best")
+
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
